c9f26969137acb565af05b8b758326b57b7bcd6b
johnzammit <40344687+johnzammit@users.noreply.github.com>
2022-10-02 16:04:49 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import React from 'react';
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
   return ( 
-    <>
-  <div> 
-    <Navbar />
-  </div> 
-  <ImageSlider slides={SliderData} /> 
-
+  <>
+    <div className="container">
+    <div className="contentwrap">
+    <Navbar />, 
+    <ImageSlider slides={SliderData} /> 
+    </div>
+    <Footer/>
+    </div>
   </>
    );
 }
